793628ca16ce214670b76020e566deced91f9b3e
rajnish <rajnish@appworkssolutions.com>
2020-05-04 10:18:37 +0530

    Department and gst module updated with toaster message and confirm dialog box

--- src/app/company/general-edit/general-edit.component.ts
+++ src/app/company/general-edit/general-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { FormBuilder, Validators } from '@angular/forms'
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ToastService } from "../../services/toast.service";
 
 @Component({
   selector: 'app-general-edit',
@@ -12,18 +13,17 @@ export class GeneralEditComponent implements OnInit {
 
   constructor(private fb: FormBuilder, private apiService: ApiService,
     public config: DynamicDialogConfig,
-    private ref: DynamicDialogRef) { }
+    private ref: DynamicDialogRef, private toastService: ToastService) { }
 
   ngOnInit(): void {
-    //if (this.config.data.iStatusID != undefined) {
-    this.GeneralSubmit.setValue({
+    this.GeneralSubmit.patchValue({
       website: this.config.data.sWebsite,
       tele1: this.config.data.sTelNo1,
       tele2: this.config.data.sTelNo2,
       fax: this.config.data.sFaxNo
     });
-    //}
   }
+
   GeneralSubmit = this.fb.group({
     website: ['', Validators.required],
     tele1: ['', Validators.required],
@@ -52,6 +52,7 @@ export class GeneralEditComponent implements OnInit {
     this.apiService.callPostApi(general_submit_data).subscribe(
       data => {
         console.log(data);
+        this.toastService.addSingle("success", "Record Added Successfully", "");
 
       },
       error => console.log(error)
